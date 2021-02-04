97117d6fd9de4278ac94f11f47c4479b6f5a7edd
davidsmorton <davidsmorton@gmail.com>
2021-02-04 10:39:30 -0700

    FCC Basic Algorithm

--- Free_code_camp/JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/09_Finders_Keepers_Array_Function.js
+++ Free_code_camp/JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/09_Finders_Keepers_Array_Function.js
@@ -1,9 +1,16 @@
 function findElement(arr, func) {
-  for (let i = 0; i < arr.length; i++){
-    console.log(arr[i]) 
-  }
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+
   
-  
-  }
-  
-  findElement([1, 2, 3, 4], num => num % 2 === 0);
+}
+return undefined;
+}
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
