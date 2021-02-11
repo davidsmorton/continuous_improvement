9de97ac66b40c0745e33d0680fa690cf36ef52fe
davidsmorton <davidsmorton@gmail.com>
2021-02-11 09:25:19 -0700

    FCC Algorithms Complete

---
+++ Free_code_camp/JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/15_Mutations.js
@@ -0,0 +1,13 @@
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+//First we make the two strings in the array lowercase. test will hold what we are looking for in target.
+// Then we loop through our test characters and if any of them is not found we return false.
+
+// If they are all found, the loop will finish without returning anything and we get to return true.
