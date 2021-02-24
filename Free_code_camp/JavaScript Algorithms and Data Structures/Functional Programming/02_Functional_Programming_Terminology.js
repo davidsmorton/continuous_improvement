99098806058625bab3e134297b8be046d032c39a
davidsmorton <davidsmorton@gmail.com>
2021-02-24 09:02:37 -0700

    FCC Functional Programming

---
+++ Free_code_camp/JavaScript Algorithms and Data Structures/Functional Programming/02_Functional_Programming_Terminology.js
@@ -0,0 +1,37 @@
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+//Functions that can be assigned to a variable, passed into another function, or returned from another function just like any other normal value, are called first class functions. In JavaScript, all functions are first class functions.
+
+// The functions that take a function as an argument, or return a function as a return value are called higher order functions.
+
+// When the functions are passed in to another function or returned from another function, then those functions which gets passed in or returned can be called a lambda.
+
