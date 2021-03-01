953e88e8a1f3bf02c58150acbd32519ab6152160
davidsmorton <davidsmorton@gmail.com>
2021-03-01 15:18:53 -0700

    FCC

---
+++ Free_code_camp/JavaScript Algorithms and Data Structures/Functional Programming/04_Avoid_Mutations_Functional_Programming.js
@@ -0,0 +1,13 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+let plusOne = fixedValue + 1; 
+console.log(plusOne)
+return plusOne
+
+  // Only change code above this line
+}
+
+// bit a trouble sorting out += this seems to change the fixedValue but "1 + "
