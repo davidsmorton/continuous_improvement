0f98e1f7512b6cb6405b702747b939467db36aa8
davidsmorton <davidsmorton@gmail.com>
2021-03-04 09:01:18 -0700

    FCC Functional Programming

---
+++ Free_code_camp/JavaScript Algorithms and Data Structures/Functional Programming/05_Avoid_External_Dependence.js
@@ -0,0 +1,12 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer (fixedValue) {
+  let plusOne = fixedValue + 1; 
+console.log(plusOne)
+return plusOne
+
+
+  // Only change code above this line
+}
